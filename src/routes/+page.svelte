2746abec5062c60b17065b9bf26a52905a2fb46f
Filina Nurcahya-Tjoa <83178463+filinanurcahya0000@users.noreply.github.com>
2024-10-24 17:10:31 -0500

    Hi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1> Filina Nurcahya-Tjoa </h1>
+<p> Hi, current data science graduate student at UC San Diego and big data engineer at AT&T.</p>
+<img src="images/IMG_1638copy.jpg" alt="This is my favorite mushroom!" height="1000" width="1000">
+<p>I currently live in Dallas, TX but what is the fun in that? </p>
+
+<script src="global.js" type="module"></script>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,15 @@
+       
+    <h1>This is my contact page!</h1>
+    <form action="mailto:filinanurcahya0000@gmail.com" method="POST" enctype="text/plain">
+        <label for="email">Your Email:</label>
+        <input id="email" name="email" type="email" required>
+        
+        <label for="subject">Subject:</label>
+        <input id="subject" name="subject" required>
+        
+        <label for="body">Message:</label>
+        <textarea id="body" name="body" rows="5" cols="30" required></textarea>
+        
+        <button type="submit">Submit</button>
+    </form>
+ 
