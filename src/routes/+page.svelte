c7ee9cc3112398ce9990301ce8e3d91383604aae
Filina Nurcahya-Tjoa <83178463+filinanurcahya0000@users.noreply.github.com>
2024-10-25 19:47:34 -0500

    x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,3 +6,17 @@
 <p> Hi, current data science graduate student at UC San Diego and big data engineer at AT&T.</p>
 <img src="images/IMG_1638copy.jpg" alt="This is my favorite mushroom!" height="1000" width="1000">
 <p>I currently live in Dallas, TX but what is the fun in that? </p>
+<h2>Latest Projects</h2>
+        <div class="projects highlights">
+            {#each projects.slice(0, 3) as project}
+                <Project data={project} hLevel={3} />
+            {/each}
+        </div>
+        <style>
+            .projects.highlights {
+                margin-top: 2rem;
+                display: grid;
+                grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+                gap: 1rem;
+            }
+        </style>
