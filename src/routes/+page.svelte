3eac0a3d0bd58226e5483ca4c6e3c0b423603fc3
Filina Nurcahya-Tjoa <83178463+filinanurcahya0000@users.noreply.github.com>
2024-10-25 19:52:01 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <svelte:head>
   <title>Highlight, this is my introduction page!</title>
 </svelte:head>
-
+<script>
+    import Project from '$lib/Project.svelte';
+    import projects from '$lib/projects.json';
+    </script>
+    
 <h1> Filina Nurcahya-Tjoa </h1>
 <p> Hi, current data science graduate student at UC San Diego and big data engineer at AT&T.</p>
 <img src="images/IMG_1638copy.jpg" alt="This is my favorite mushroom!" height="1000" width="1000">
 <p>I currently live in Dallas, TX but what is the fun in that? </p>
+
 <h2>Latest Projects</h2>
         <div class="projects highlights">
             {#each projects.slice(0, 3) as project}
